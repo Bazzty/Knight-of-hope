6077ee0e7b7d0ce804838b5e961694746fb11af8
Bastian Contreras <bastiansolis123@gmail.com>
2026-06-20 21:13:48 -0400

    fix: configurar cors, limpiar server.js y reforzar gitignore

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-// Middlewares
+app.use(cors());
 app.use(express.json());
 
-// Rutas base
 app.get('/', (req, res) => {
     res.send('¡Backend de Knight of Hope configurado correctamente!');
 });
 
-// Exportamos la app, NO usamos app.listen aquí
 module.exports = app;
